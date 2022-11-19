9841e8798b11a6d9e335b3887b64725caf8dcb70
pvshenoy <91280014+pvshenoy@users.noreply.github.com>
2022-11-18 23:50:35 -0500

    Final Description Edits

---
+++ idm241/final/build/script.js
@@ -0,0 +1,21 @@
+// var eyeIcon = document.getElementById("blue");
+//     eyeIcon.onmouseover = mouseOver;
+//     eyeIcon.onmouseout = mouseOut;
+    
+//     function mouseOver() { 
+//       eyeIcon.src = "../images/eye_icon-hover.svg";
+//     }
+//     function mouseOut() { 
+//       eyeIcon.src = "../images/eye-icon.svg";
+//     }
+
+// var reviewIcon = document.getElementById("green");
+//     reviewIcon.onmouseover = mouseOver;
+//     reviewIcon.onmouseout = mouseOut;
+    
+//     function mouseOver() { 
+//       reviewIcon.src = "../images/review_icon-hover.png";
+//     }
+//     function mouseOut() { 
+//       reviewIcon.src = "../images/review-icon.svg";
+//     }
